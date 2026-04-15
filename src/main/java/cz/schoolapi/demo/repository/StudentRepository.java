25b978ad5cda87c54c4af77facce045367319305
DjangoCzech <janpanus@gmail.com>
2026-04-15 09:04:51 +0200

    Start projektu

--- src/main/java/cz/schoolapi/demo/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package cz.schoolapi.demo.repository;
-
-import cz.schoolapi.demo.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
